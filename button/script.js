b7c034f2c91073389ee97787707e1607bdb867cf
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-04-30 17:22:07 +0300

    html&css

---
+++ button/script.js
@@ -0,0 +1,19 @@
+const toggle = document.getElementById('themeToggle');
+
+// при зареждане
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark');
+  toggle.checked = true;
+}
+
+// при смяна
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    document.body.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  }
+});
+

352084b290fd391ea02c605322623575915960d1
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-05-02 12:21:03 +0300

    html&css

--- button/script.js
+++ button/script.js
@@ -8,9 +8,11 @@ if (localStorage.getItem('theme') === 'dark') {
 
 // при смяна
 toggle.addEventListener('change', () => {
+
   if (toggle.checked) {
     document.body.classList.add('dark');
     localStorage.setItem('theme', 'dark');
+    
   } else {
     document.body.classList.remove('dark');
     localStorage.setItem('theme', 'light');
